35b3afb7dbea6f63fc99bdf7b29a6c7c86b6abb4
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-06-08 00:02:30 +0000

    added new post event for the app

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -9,6 +11,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -36,3 +41,14 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok")
+})
+
+
+function generateRandomString() {
+
+}
